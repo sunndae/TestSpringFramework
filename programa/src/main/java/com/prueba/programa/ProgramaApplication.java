2eaad27b21bd783632b658c74098c81b0987e514
sunndae <tomassolar478@gmail.com>
2024-07-10 23:30:04 -0400

    Primer commit

---
+++ programa/src/main/java/com/prueba/programa/ProgramaApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.programa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramaApplication.class, args);
+	}
+
+}
